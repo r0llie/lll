7ff889391152e4bbbf752e4942264cb642b2be34
rollienodes <158350450+r0llie@users.noreply.github.com>
2025-07-12 18:05:09 +0300

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    ua = request.headers.get('User-Agent')
+    time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"{time} - IP: {ip} - UA: {ua}")
+    return "<h1>Logged!</h1>", 200
+
+if __name__ == '__main__':
+    app.run()
