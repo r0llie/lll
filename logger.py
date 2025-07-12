424b22663956050c1d00f6503adcb9c8d5c45a3c
rollienodes <158350450+r0llie@users.noreply.github.com>
2025-07-12 18:12:50 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import datetime
 
@@ -12,4 +13,5 @@ def index():
     return "<h1>Logged!</h1>", 200
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
